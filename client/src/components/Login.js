ced7ac29bf79fc176f5da5d539cd79002cc53efd
Angela Flowers <me@angela.flowers>
2019-10-18 11:45:57 -0400

    built login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: ""
+  })
+
+  const handleChange = event => {
+    setCredentials({
+      ...credentials,
+      [event.target.name]: event.target.value
+    })
+  }
+
+  const login = event => {
+    event.preventDefault();
+    axiosWithAuth()
+      .post("/api/login", credentials)
+      .then(res => console.log(res))
+      .catch(err => console.log(err))
+  }
+
+
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+    <div className="login-form">
+      <h2>React Bubbles Login</h2>
+      <form onSubmit={login}>
+        <label>
+          Username:
+          <input
+            type="text"
+            name="username"
+            value={credentials.username}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            name="password"
+            value={credentials.password}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <button>Log In</button>
+      </form>
+    </div>
   );
 };
 
-export default Login;
+
+  export default Login;
